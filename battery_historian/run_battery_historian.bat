98448c24c4efacc181d57532026a82551690b4e2
DESKTOP-HSULG0U\luo <luohaishun@126.com>
2017-06-06 14:33:33 +0800

    修改不能启动battery-historian.go 的问题

--- battery_historian/run_battery_historian.bat
+++ battery_historian/run_battery_historian.bat
@@ -1,1 +1,3 @@
-D: &cd \Program Files\GoData\src\github.com\google\battery-historian\cmd\battery-historian &start go run battery-historian.go
+D: &cd \Program Files\GoData\src\github.com\google\battery-historian\  & go run cmd\battery-historian\battery-historian.go
+
+pause
